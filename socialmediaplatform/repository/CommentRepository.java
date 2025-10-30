6b24636aaffbb18d677bf9a2ee1f79688faa7171
mingennnnn <mingenchoon@gmail.com>
2025-10-30 16:22:02 +0800

    Delete socialmediaplatform directory

--- socialmediaplatform/repository/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miniproject.socialmediaplatform.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.miniproject.socialmediaplatform.model.Comment;
-
-public interface CommentRepository extends MongoRepository<Comment, String> {
-}
-
--- socialmediaplatform/repository/ProfileRepository.java
+++
@@ -1,8 +0,0 @@
-package com.miniproject.socialmediaplatform.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.miniproject.socialmediaplatform.model.Profile;
-
-public interface ProfileRepository extends MongoRepository<Profile, String> {
-    Profile findProfileByUsername(String username);
-}
